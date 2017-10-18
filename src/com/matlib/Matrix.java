b2fbec7cde8c53b5e41f5fb2f3c12fd610f84388
Sarath <nsarathsoman@gmail.com>
2017-10-18 23:11:28 +0530

    inverse method, Gauss Jordan added

--- src/com/matlib/Matrix.java
+++ src/com/matlib/Matrix.java
@@ -4,22 +4,22 @@ public class Matrix {
 
     private double m[][];
 
-    private long rows;
-    private long cols;
+    private int rows;
+    private int cols;
 
-    public long rows() {
+    public int rows() {
         return rows;
     }
 
-    public void rows(long rows) {
+    public void rows(int rows) {
         this.rows = rows;
     }
 
-    public long cols() {
+    public int cols() {
         return cols;
     }
 
-    public void cols(long cols) {
+    public void cols(int cols) {
         this.cols = cols;
     }
 
@@ -55,4 +55,111 @@ public class Matrix {
         m[i][i] = element;
         return this;
     }
+
+    public Matrix inverse() {
+        double[][] augmentedMatrix = getAugmentedMatrix();
+        Matrix augMatrix = new Matrix(augmentedMatrix);
+        return findInverse(augMatrix);
+    }
+
+    private double[][] getAugmentedMatrix() {
+        if(rows != cols) {
+            return null;
+        }
+        double[][] augmentedMatrix = new double[rows][cols + rows];
+
+        augmentMatrixWithIdentityMatrix(this, augmentedMatrix);
+
+        return augmentedMatrix;
+    }
+
+    private static void augmentMatrixWithIdentityMatrix(Matrix matrix, double[][] augmentedMatrix) {
+        for (int i = 0; i < matrix.rows; i++) {
+
+            for (int j = 0; j < matrix.cols; j++) {
+                augmentedMatrix[i][j] = matrix.element(i, j);
+            }
+
+            for(int j = matrix.cols; j < matrix.rows + matrix.cols; j++) {
+                if(i == (j - matrix.cols)) {
+                    augmentedMatrix[i][j] = 1;
+                } else {
+                    augmentedMatrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    private static Matrix findInverse(Matrix augmentedMatrix) {
+        for (int k = 0; k < augmentedMatrix.rows; k++) {
+            dumpToConsole(augmentedMatrix);
+            double coeff = augmentedMatrix.element(k, k);
+            if (0 == coeff) {
+//                System.out.printf("Coeffient of cell [%d][%d] is zero \n", k, k);
+                int newIndexOfK = reorderEquation(augmentedMatrix, k);
+                if(newIndexOfK < 0) {
+                    throw new RuntimeException("No swappable equations found");
+                }
+                System.out.printf("swapped row %d with %d\n", k, newIndexOfK);
+                coeff = augmentedMatrix.element(k, k);
+            }
+
+            for (int j = 0; j < augmentedMatrix.cols; j++) {
+                augmentedMatrix.element(k, j, augmentedMatrix.element(k, j) / coeff );
+            }
+            for (int i = 0; i < augmentedMatrix.rows; i++) {
+                if (k == i) continue;
+                for (int j = k + 1; j < augmentedMatrix.cols; j++) {
+                    augmentedMatrix.element(i, j,
+                            augmentedMatrix.element(i, j) -
+                                    (augmentedMatrix.element(k, j) * augmentedMatrix.element(i, k)));
+                }
+            }
+        }
+
+        double[][] inverse = new double[augmentedMatrix.rows][augmentedMatrix.rows];
+        for(int i = 0; i < augmentedMatrix.rows; i++) {
+            for (int j = augmentedMatrix.rows; j < augmentedMatrix.cols; j++) {
+                inverse[i][j] = augmentedMatrix.element(i, j);
+            }
+        }
+
+        return new Matrix(inverse);
+    }
+
+    private static int reorderEquation(Matrix augmentedMatrix, int k) {
+        int newIndexOfK = -1;
+        for(int i = k + 1; i < augmentedMatrix.rows; i++) {
+            if(augmentedMatrix.element(i, i) != 0) {
+                newIndexOfK = i;
+                swapRows(augmentedMatrix, k, newIndexOfK);
+                break;
+            }
+        }
+
+        return newIndexOfK;
+    }
+
+    private static void swapRows(Matrix matrix, int k, int newIndexOfK) {
+        for(int i = 0; i < matrix.cols; i++) {
+            matrix.element(k, i, matrix.element(k, i) + matrix.element(newIndexOfK, i));
+            matrix.element(newIndexOfK, i, matrix.element(k, i) - matrix.element(newIndexOfK, i));
+            matrix.element(k, i, matrix.element(k, i) - matrix.element(newIndexOfK, i));
+        }
+    }
+
+    private static void dumpToConsole(Matrix matrix) {
+        for (int i = 0; i < matrix.rows; i++) {
+            for (int j = 0; j < matrix.cols; j++) {
+                if (j == matrix.rows) {
+                    System.out.printf("  |  %f ", matrix.element(i, j));
+                } else {
+                    System.out.printf("  %f  ", matrix.element(i, j));
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
 }
