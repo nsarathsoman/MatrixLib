7008192186306f3108344b151e03749adf23c291
Sarath <nsarathsoman@gmail.com>
2017-10-18 08:01:18 +0530

    Matrix basic structure

--- src/com/matlib/Matrix.java
+++ src/com/matlib/Matrix.java
@@ -7,4 +7,52 @@ public class Matrix {
     private long rows;
     private long cols;
 
+    public long rows() {
+        return rows;
+    }
+
+    public void rows(long rows) {
+        this.rows = rows;
+    }
+
+    public long cols() {
+        return cols;
+    }
+
+    public void cols(long cols) {
+        this.cols = cols;
+    }
+
+    public Matrix(double[][] m) {
+        this.m = m;
+        rows = m.length;
+        if(rows > 0) {
+            cols = m[0].length;
+        }
+    }
+
+    public double element(int i, int j) {
+        if(i >= rows) {
+            throw new IndexOutOfBoundsException("Given index i: " + i + " is out of bound");
+        }
+
+        if(j >= cols) {
+            throw new IndexOutOfBoundsException("Given index j: " + j + " is out of bound");
+        }
+
+        return m[i][j];
+    }
+
+    public Matrix element(int i, int j, double element) {
+        if(i >= rows) {
+            throw new IndexOutOfBoundsException("Given index i: " + i + " is out of bound");
+        }
+
+        if(j >= cols) {
+            throw new IndexOutOfBoundsException("Given index j: " + j + " is out of bound");
+        }
+
+        m[i][i] = element;
+        return this;
+    }
 }
